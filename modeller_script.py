0d78423fcf5c9444d0c5e0f0d83dc0fc38b5278b
jhshin0714 <tlswhdgusd@gmail.com>
2024-07-30 14:05:49 +0900

    modeller_script.py

--- modeller_script.py
+++ modeller_script.py
@@ -5,7 +5,7 @@ import sys
 
 log.verbose()
 env = Environ()
-env.io.atom_files_directory = ['/home/avian-flu/Desktop/Ag_Shift/modeller/atom_files']
+env.io.atom_files_directory = ['input/your/atom/files']
 a = AutoModel(env,alnfile='{}'.format(sys.argv[1]),knowns='{}'.format(sys.argv[2]),sequence='{}'.format(sys.argv[3]))
 a.starting_model=1
 a.ending_model=1
